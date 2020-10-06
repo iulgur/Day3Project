d8321c7e8a9b5625bc1ae248b6fa80cab44bd5c4
Ilyas Ulgur <ulgur@yahoo.com>
2020-10-06 21:46:13 +0200

    added new file to awesome feature

---
+++ src/test/java/AwesomeTest.java
@@ -0,0 +1,9 @@
+public class AwesomeTest {
+
+    public static void main(String[] args) {
+        //red means it's untracked
+        //green means it's under stating area
+        //grey in .idea folder means it's ignored.
+        //when the colour is normal, it means it's committed no changes.
+    }
+}
