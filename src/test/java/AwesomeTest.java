22ac054e02353c46003fa53904c492695f9a6823
Ilyas Ulgur <ulgur@yahoo.com>
2020-10-06 21:59:13 +0200

    added more awesome staff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -5,5 +5,11 @@ public class AwesomeTest {
         //green means it's under stating area
         //grey in .idea folder means it's ignored.
         //when the colour is normal, it means it's committed no changes.
+
+
+        //I just pulled the new change from GitHub that I did not have locally and now my project
+        //is in sync with remote repository
+
+        //Now I made more awesome changes and I am about to push this back to GitHub.
     }
 }
