df921eb6863406d3625f80341909c8be621925d8
Ilyas Ulgur <ulgur@yahoo.com>
2020-10-06 22:41:28 +0200

    first hallo file

---
+++ src/test/java/iulgur/Hallo.java
@@ -0,0 +1,17 @@
+package iulgur;
+
+public class Hallo {
+    public static void main(String[] args) {
+        // TASK FINAL :
+        /*
+         * 1. Go to Github , fork my repo into your account
+         * 2. (new) Clone the forked repo from your account into intelliJ
+         * 3. add a folder under src/test/java with your github username
+         * 4. add a class under this username called Hello
+         * 5. push it back to your own GitHub
+         * 6. (new) request the owner of original repo to add your change to original repo
+         *      by opening pull request ( request to merge your change into original repo)
+         * 7. observe how it might look like in owner's repo
+         * */
+    }
+}
